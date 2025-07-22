7a4cdce4923f93c610daef86c273c5226b52909c
Vivi <sun20317@gmail.com>
2025-07-23 04:26:38 +0800

    fix: refactor frontend URL handling in redirect logic for consistency

--- src/routes/redirect.js
+++ src/routes/redirect.js
@@ -5,7 +5,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const router = express.Router();
-
+const frontendUrl = process.env.FRONTEND_BASE_URL || "http://localhost:5173";
 // Redirect from short code to original URL
 router.get("/:shortCode", async (req, res) => {
   const { shortCode } = req.params;
@@ -22,8 +22,7 @@ router.get("/:shortCode", async (req, res) => {
 
     if (url.password) {
       // Redirect to frontends's protected link page
-      const frontendUrl =
-        process.env.FRONTEND_BASE_URL || "http://localhost:5173";
+
       return res.redirect(`${frontendUrl}/protected-link/${shortCode}`);
     }
     // Basic validation for originalUrl before redirection to prevent open redirect vulnerabilities. should be expanded
@@ -36,10 +35,10 @@ router.get("/:shortCode", async (req, res) => {
       console.error("Invalid originalUrl for redirection:", err);
       return res.status(400).json({ error: "Invalid URL for redirection" });
     }
-    res.redirect(url.originalUrl);
+    res.redirect(`${frontendUrl}/${shortCode}`);
   } catch (error) {
     console.error(error);
-      return res.status(500).json({ error: "Invalid URL for redirection" });
+    return res.status(500).json({ error: "Invalid URL for redirection" });
   }
 });
 
